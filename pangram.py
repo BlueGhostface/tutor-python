b43cbb0410a36fcbdbf7aeba0fbb337f425679a3
axell.tielemans <axelltielemans@gmail.com>
2025-02-21 19:24:53 +0100

    first python files

---
+++ pangram.py
@@ -0,0 +1,21 @@
+﻿
+testpangram = "The quick brown fox jumps over the lazy dog"
+testnotpangram = "The quick brown cat jumps over the lazy cat"
+
+
+
+def is_pangram(st):
+    f=0
+    if len(st) < 26:
+        return False
+    for char in st.lower():
+        if char.isalpha() :
+            f |= 1 << (ord(char) - ord('a'))
+
+        if f == (1<<26 ) -1:
+            return True
+    return False
+
+
+print(is_pangram(testpangram))
+print(is_pangram(testnotpangram))
---
+++ testfile.py
@@ -0,0 +1,10 @@
+﻿lamp = "online"
+cond = True
+age = 24
+price = 10.99
+
+
+def test():
+    print("Hello, world! This is a test." + str(lamp))
+    print(f"is this a test? {cond}")
+test()
