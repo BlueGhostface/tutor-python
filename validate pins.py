678d350e5ab09d74a18bb4f880ffb6253ae7bc82
axell.tielemans <axelltielemans@gmail.com>
2025-02-22 21:58:57 +0100

    problem validating a pin code, exact 4 or 6 digits

---
+++ validate pins.py
@@ -0,0 +1,18 @@
+﻿import re
+
+
+
+def validate_pin(pin):
+    return bool(re.fullmatch(r"\d{4}|\d{6}", pin))
+
+
+print(validate_pin("1"))
+print(validate_pin("12"))
+print(validate_pin("1234"))
+
+print(validate_pin("1234\n"))
+print(validate_pin("123456\n"))
+
+print(validate_pin("12345"))
+print(validate_pin("123456"))
+print(validate_pin("1234567"))
