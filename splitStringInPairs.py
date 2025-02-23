28fde2235d1848af3910f72edc41d6a70135b093
axell.tielemans <axelltielemans@gmail.com>
2025-02-23 21:01:38 +0100

    split strings in a pairs (append an "_" to uneven parts)

---
+++ splitStringInPairs.py
@@ -0,0 +1,16 @@
+﻿
+def split_pairs(s):
+    result = []
+    if len(s) % 2:
+        s += '_'
+    for i in range(0, len(s), 2):
+        result.append(s[i:i+2])
+    return result
+
+
+
+
+
+print(split_pairs('abc'))
+print(split_pairs('abcd'))
+print(split_pairs('abcdef'))
