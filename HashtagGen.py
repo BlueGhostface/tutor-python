cf0e8beee90ed4c709ca2f013cebb4b10fd0d128
axell.tielemans <axelltielemans@gmail.com>
2025-02-24 20:24:54 +0100

    making hashtags, removing whitespaces and capitalizing the first letters

---
+++ HashtagGen.py
@@ -0,0 +1,14 @@
+﻿def generate_hashtag(s):
+    if len(s) <= 0:
+        return False
+    s = s.strip()
+    s = "#" + s.title().replace(" ", "")
+
+    if len(s) > 140:
+        return False
+    return s
+
+print(generate_hashtag("Hello There")) # #HelloThere
+print(generate_hashtag("            hello there        ")) # #HelloThere
+print(generate_hashtag("hello thEre        ")) # #HelloThere
+print(generate_hashtag("heLLo there")) # #HelloThere
