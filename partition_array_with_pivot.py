aa670a3239eddae9c399de42b802333f7e042996
axell.tielemans <axelltielemans@gmail.com>
2025-03-03 14:17:42 +0100

    partition an array with a specific pivot value, also refactoring filenaming

---
+++ partition_array_with_pivot.py
@@ -0,0 +1,23 @@
+﻿def pivotArray(nums, pivot):
+    less = []
+    equal = []
+    greater = []
+
+    for num in nums:
+        if num < pivot:
+            less.append(num)
+        elif num > pivot:
+            greater.append(num)
+        else:
+            equal.append(num)
+
+    less.extend(equal)
+    less.extend(greater)
+
+    return less
+
+
+nums = [9, 12, 3, 5, 14, 10, 10]
+pivot = 10
+
+print(pivotArray(nums,pivot))
