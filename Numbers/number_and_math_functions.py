63087f71b66221c85406d210c68de1b421c654fe
axell.tielemans <axelltielemans@gmail.com>
2025-05-06 22:32:00 +0200

    using functions for numbers and operations

---
+++ Numbers/number_and_math_functions.py
@@ -0,0 +1,52 @@
+﻿def identity(a): return a
+
+
+def zero(f=identity): return f(0)
+
+
+def one(f=identity): return f(1)
+
+
+def two(f=identity): return f(2)
+
+
+def three(f=identity): return f(3)
+
+
+def four(f=identity): return f(4)
+
+
+def five(f=identity): return f(5)
+
+
+def six(f=identity): return f(6)
+
+
+def seven(f=identity): return f(7)
+
+
+def eight(f=identity): return f(8)
+
+
+def nine(f=identity): return f(9)
+
+
+def plus(b): return lambda a: a + b
+
+
+def minus(b): return lambda a: a - b
+
+
+def times(b): return lambda a: a * b
+
+
+def divided_by(b): return lambda a: a // b
+
+
+def basic_test_cases():
+    print(seven(times(five())))  # 35
+    print(four(plus(nine())))  # 13
+    print(eight(minus(three())))  # 5
+    print(six(divided_by(two())))  # 3
+
+basic_test_cases()
