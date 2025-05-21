10f30b98f949a461deed66cc565b656568883d6d
axell.tielemans <axelltielemans@gmail.com>
2025-05-21 18:41:05 +0200

    define a type of triangle when we know the lenghts of the sides

---
+++ problems/define_type_triangle.py
@@ -0,0 +1,21 @@
+﻿def triangleType(nums) -> str:
+    nums.sort()
+    if nums[0] + nums[1] <= nums[2]:
+        return "none"
+    elif nums[0] == nums[2]:
+        return "equilateral"
+    elif nums[0] == nums[1] or nums[1] == nums[2]:
+        return "isosceles"
+    else:
+        return "scalene"
+
+
+inputnums = [2, 2, 3]
+inputnums2 = [3, 3, 3]
+inputnums3 = [2, 1, 3]
+inputnums4 = [3,4,5]
+
+print(triangleType(inputnums))
+print(triangleType(inputnums2))
+print(triangleType(inputnums3))
+print(triangleType(inputnums4))
