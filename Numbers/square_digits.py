58988eebe7b79226ec4d9eac0fa55c85b5031001
axell.tielemans <axelltielemans@gmail.com>
2025-05-30 23:50:19 +0200

    square of digits

---
+++ Numbers/square_digits.py
@@ -0,0 +1,11 @@
+﻿def square_digits(num):
+    res = ""
+    for x in str(num):
+        res += str(int(x)**2)
+    return int(res)
+
+
+print(square_digits(9119), 811181)
+print(square_digits(0), 0)
+
+
