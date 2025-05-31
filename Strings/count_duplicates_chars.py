8f2bc89172dad4c4bdd05877033bafb68e4da14f
axell.tielemans <axelltielemans@gmail.com>
2025-05-31 20:21:11 +0200

    count duplicates of characters in a text

---
+++ Strings/count_duplicates_chars.py
@@ -0,0 +1,12 @@
+﻿def duplicate_count(text):
+    n=0
+    text=text.lower()
+    for i in set(text):
+        if text.count(i) >1:
+            n+=1
+    return n
+
+print(duplicate_count("abcde"), 0)
+print(duplicate_count("aabbcde"), 2)
+print(duplicate_count("Advertisement"), 3)
+print(duplicate_count("problems solving"), 3)
