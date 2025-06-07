e26b51b24775f3f3efcc0d24fd8532a32fb7d4fb
axell.tielemans <axelltielemans@gmail.com>
2025-06-07 22:45:48 +0200

    check if the square root of an inputted number is an integer

---
+++ problems/check_if_square_root_is_integer.py
@@ -0,0 +1,29 @@
+﻿import math
+
+def is_square(n):
+    if n >=0:
+        root = math.sqrt(n)
+        return root.is_integer()
+    return False
+
+
+
+
+def is_square2(n):
+    return n > -1 and math.sqrt(n) % 1 == 0
+
+
+
+print(is_square(-1),is_square2(-1), "false")
+print(is_square(0), is_square2(0),"true")
+print(is_square(3),is_square2(3),"false")
+print(is_square(4),is_square2(4),"true")
+print(is_square(9),is_square2(9),"true")
+print(is_square(16),is_square2(16),"true")
+print(is_square(25),is_square2(25),"true")
+
+print(is_square(30),is_square2(30),"false")
+print(is_square(48),is_square2(48),"false")
+print(is_square(81),is_square2(81),"true")
+print(is_square(100),is_square2(100),"true")
+print(is_square(105),is_square2(105),"false")
