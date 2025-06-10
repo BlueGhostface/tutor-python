bb55e7518d28f84b0e88df665d13855ca0b58f70
axell.tielemans <axelltielemans@gmail.com>
2025-06-10 20:27:21 +0200

    change a string into a list of integers and find the highest and lowest integer

---
+++ Numbers/highest_and_lowest_integers_from_string.py
@@ -0,0 +1,12 @@
+﻿def high_and_low(numbers):
+    int_list = list(map(int, numbers.split()))
+    highest = max(int_list)
+    lowest = min(int_list)
+    return f"{highest} {lowest}"
+
+
+
+
+print(high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4") == "42 -9")
+print(high_and_low("1 2 3" ) == "3 1")
+
