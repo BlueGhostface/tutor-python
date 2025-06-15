8a2d8a97dd671cf75c1a330f8392db51cf12a8c0
axell.tielemans <axelltielemans@gmail.com>
2025-06-15 22:52:37 +0200

    strips all text from that follows a set of comment markers

---
+++ strip_comments_from_string.py
@@ -0,0 +1,10 @@
+﻿def solution(string,markers):
+    parts = string.split('\n')
+    for s in markers:
+        parts = [v.split(s)[0].rstrip() for v in parts]
+    return '\n'.join(parts)
+
+
+print(solution('apples, pears # and bananas\ngrapes\nbananas !apples', ['#', '!']), 'apples, pears\ngrapes\nbananas')
+print(solution('apples pears bananas // otherfruit', ['/']), 'apples pears bananas')
+print(solution('pineapple $ apple ', ['$']), ' pineapple')
