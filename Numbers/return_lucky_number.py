a46426366b6dc9ead443e613790c8186d1ea9c56
axell.tielemans <axelltielemans@gmail.com>
2025-07-05 17:01:11 +0200

    return the number whose frequency in the array equals its value

---
+++ Numbers/return_lucky_number.py
@@ -0,0 +1,16 @@
+﻿from collections import Counter
+
+
+def findLucky(nums) -> int:
+    freq = Counter(nums)
+    lucky = -1
+    for num, count in freq.items():
+        if num == count:
+            lucky = max(lucky, num)
+    return lucky
+
+
+# Example usage:
+print(findLucky([2, 2, 3, 4]))  # Output: -1
+print(findLucky([1, 2, 2, 3, 3, 3]))  # Output: 3
+print(findLucky([2, 2, 3, 4, 4, 4, 4]))  # Output: 4
