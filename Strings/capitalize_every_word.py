22b8c77b64d1b7fd9f2a6449a1404c1dd2e94d69
axell.tielemans <axelltielemans@gmail.com>
2025-07-08 21:41:07 +0200

    capitalize every word in an inputed text

---
+++ Strings/capitalize_every_word.py
@@ -0,0 +1,9 @@
+﻿def capitalize_text(string):
+    list = string.split()
+    new_list = [i.capitalize() for i in list]
+    return ' '.join(new_list)
+
+# Test cases
+print(capitalize_text("hello world"))
+print(capitalize_text("python programming is fun"))
+print(capitalize_text("capitalize every word in this sentence"))
