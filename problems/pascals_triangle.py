671188db22906604f38fd8fa6449fc95fad9ee33
axell.tielemans <axelltielemans@gmail.com>
2025-08-02 21:52:00 +0200

    output the result of x rows of a pascal's triangle

---
+++ problems/pascals_triangle.py
@@ -0,0 +1,26 @@
+﻿# give every row of Pascal's triangle as a list
+
+# example
+#     1
+#    1 1
+#   1 2 1
+#  1 3 3 1
+# 1 4 6 4 1
+
+
+
+def generate( numRows):
+    pascal = []
+
+    for i in range(numRows):
+        row = [1] * (i + 1)
+        for j in range(1, i):
+            row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+        pascal.append(row)
+
+    return pascal
+
+
+print(generate(5))
+print(generate(0))
+print(generate(3))
